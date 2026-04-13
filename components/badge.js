fba5b54cd024ee5911aa174fbb6c9b4d977f7390
rosenrose <crazytempler@gmail.com>
2026-04-14 08:13:26 +0900

    Adding Children

---
+++ components/badge.js
@@ -0,0 +1,42 @@
+class BadgeComponent extends HTMLElement {
+    #span;
+
+    connectedCallback() {
+        if (!this.#span) {
+            this.#span = Object.assign(document.createElement("span"), { className: "x-badge-label" });
+        }
+
+        this.prepend(this.#span);
+        this.update();
+    }
+
+    static get observedAttributes() {
+        return ["content"];
+    }
+
+    attributeChangedCallback() {
+        this.update();
+    }
+
+    set content(value) {
+        if (this.getAttribute("content") === value) {
+            return;
+        }
+
+        this.setAttribute("content", value);
+    }
+
+    get content() {
+        return this.getAttribute("content");
+    }
+
+    update() {
+        if (!this.#span) {
+            return;
+        }
+
+        this.#span.textContent = this.getAttribute("content");
+    }
+}
+
+export const registerBadgeComponent = () => customElements.define("x-badge", BadgeComponent);
--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 import { registerAvatarComponent } from "./components/avatar.js";
+import { registerBadgeComponent } from "./components/badge.js";
 
 const app = () => {
     registerAvatarComponent();
+    registerBadgeComponent();
 }
 
 document.addEventListener("DOMContentLoaded", app);
+
+document.querySelector("#badge-input").addEventListener("input", (event) => {
+    const num = parseInt(event.target.value);
+    const min = parseInt(event.target.min);
+    const max = parseInt(event.target.max);
+
+    if (isNaN(num)) {
+        event.target.value = "";
+    }
+    else if (num < min) {
+        event.target.value = min;
+    }
+    else if (num > max) {
+        event.target.value = max;
+    }
+
+    document.querySelector("#live-badge").content = event.target.value;
+});
